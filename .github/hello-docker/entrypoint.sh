f7498ff6f8ef287189cad2a050e7d6d377159c8e
aliu112 <liu.aaron835@gmail.com>
2024-02-20 16:09:16 -0500

    docker github action

---
+++ .github/hello-docker/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "::debug::Running entrypoint.sh"
+echo "Hello $1"
+echo "INPUT_WHO_TO_GREET: $INPUT_WHO_TO_GREET"
+echo "Hello: $HELLO"
+time=$(data)
+echo "time=$time" >> $GITHUB_OUTPUT
+echo "HELLO_TIME=$time" >> $GITHUB_ENV
